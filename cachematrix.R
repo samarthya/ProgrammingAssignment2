c1684851685cf0c6f0f690a940f175fc8de9f3a2
Saurabh Sharma <10424261+samarthya@users.noreply.github.com>
2025-01-23 16:05:42 -0500

    Update cachematrix.R
         [,1] [,2]
    [1,]    1    3
    [2,]    2    4
         [,1] [,2]
    [1,]   -2  1.5
    [2,]    1 -0.5
         [,1] [,2]
    [1,]   -2  1.5
    [2,]    1 -0.5
    Getting cached inverse.
         [,1] [,2]
    [1,]   -2  1.5
    [2,]    1 -0.5

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- cacheMatrix$getInverse()
+
+  if (!is.null(inv)) {
+    message("Getting cached inverse.")
+    return(inv)
+  }
+
+  mat <- cacheMatrix$get()
+  inv <- solve(mat)
+
+
+  cacheMatrix$setInverse(inv)
+
+  inv
 }
